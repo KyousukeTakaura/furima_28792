38a679cf11071801f4a283e82d276b0b79fb8aab
KyousukeTakaura <0xs6631.3261c6r@ezweb.ne.jp>
2020-09-01 19:15:01 +0900

    ルボコップを使った

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  with_options presence:true do       
-  validates :nickname
-  validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
-  validates :family_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
-  validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
-  validates :birth_day
-  end
 
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください。' }
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください。' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。' }
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。' }
+    validates :birth_day
+  end
 end
