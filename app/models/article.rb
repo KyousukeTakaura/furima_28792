11804a7f716f3ced2c6bef830129a6f429d3d125
KyousukeTakaura <0xs6631.3261c6r@ezweb.ne.jp>
2020-09-02 13:01:55 +0900

    purchase

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,12 @@
 class Article < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :genre
+  belongs_to_active_hash :purchase
 
   # 空の投稿を保存できないようにする
-  validates :title, :text, :genre, presence: true
+  validates :title, :text, :purchase, presence: true
 
   # 選択が「--」のままになっていないか
   with_options numericality: { other_than: 1 } do
-    validates :genre_id
+    validates :purchase_id
   end
 end
