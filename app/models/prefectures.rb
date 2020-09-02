acfba8b20947f5bff029ddb85fc0aa051b4d546f
KyousukeTakaura <0xs6631.3261c6r@ezweb.ne.jp>
2020-09-02 12:19:41 +0900

    ジャンルの変更

--- app/models/prefectures.rb
+++ app/models/prefectures.rb
@@ -1,5 +1,5 @@
-class Genre < ActiveHash::Base
-  prefectures.data = [
+class Prefectures < ActiveHash::Base
+  self.data = [
     { id: 1, name: '--' },
     { id: 1, name: '北海道' },
     { id: 2, name: '青森県' },
